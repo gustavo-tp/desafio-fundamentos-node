d3b352b894ea072ac1d8ba2ec84986d0fa1c55c4
Gustavo Turra Perandré <gustavo1190.gt@gmail.com>
2020-07-17 10:15:44 -0300

    :sparkles: chalenge finalized

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,45 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const incomeTransactions = this.transactions.filter(
+      transaction => transaction.type === 'income',
+    );
+
+    const outcomeTransactions = this.transactions.filter(
+      transaction => transaction.type === 'outcome',
+    );
+
+    const totalIncome = incomeTransactions.reduce(
+      (amount, transaction) => amount + transaction.value,
+      0,
+    );
+
+    const totalOutcome = outcomeTransactions.reduce(
+      (amount, transaction) => amount + transaction.value,
+      0,
+    );
+
+    const total = totalIncome - totalOutcome;
+
+    const balance = {
+      income: totalIncome,
+      outcome: totalOutcome,
+      total,
+    };
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, type, value }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
